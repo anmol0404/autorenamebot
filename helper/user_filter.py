671354781a86e66ba354e96d081aa4c308572592
root <root@srv1293970.hstgr.cloud>
2026-06-20 15:34:58 +0000

    Fixed admin filter: added authSource, try-except in filter

--- helper/user_filter.py
+++ helper/user_filter.py
@@ -3,21 +3,23 @@ from config import Config
 from helper.database import db
 
 async def admin_check(_, __, message):
-    if not message.from_user:
-        return False
-    
-    # Check if bot is in public mode via Env or DB
-    if Config.IS_PUBLIC:
-        return True
+    try:
+        if not message.from_user:
+            return False
         
-    is_public = await db.get_config('is_public', False)
-    if is_public:
-        return True
-        
-    if message.from_user.id in Config.ADMIN:
-        return True
+        if Config.IS_PUBLIC:
+            return True
+            
+        is_public = await db.get_config('is_public', False)
+        if is_public:
+            return True
+            
+        if message.from_user.id in Config.ADMIN:
+            return True
 
-    db_admins = await db.get_db_admins()
-    return message.from_user.id in db_admins
+        db_admins = await db.get_db_admins()
+        return message.from_user.id in db_admins
+    except:
+        return False
 
 admin_only = filters.create(admin_check)
